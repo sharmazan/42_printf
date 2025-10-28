d97cbdfed097b8d9a5a792eff2dae759df3f3809
Sergii Sharmazanov <ssharmaz@c2r2p8.42vienna.com>
2025-10-28 19:33:25 +0100

    First dirty version

---
+++ Makefile
@@ -0,0 +1,25 @@
+NAME = libftprintf.a
+HEADER = libftprintf.h
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+SOURCES =	ft_printf.c
+OBJECTS = $(SOURCES:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJECTS)
+	ar rcs $(NAME) $?
+
+%.o: %.c $(HEADER)
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+fclean: clean
+	rm -f $(NAME)
+
+clean:
+	rm -f $(OBJECTS)
+
+re: fclean all
+
+.PHONY: all clean tests re fclean
---
+++ ft_printf.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssharmaz <ssharmaz@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 23:17:00 by ssharmaz          #+#    #+#             */
+/*   Updated: 2025/10/28 19:32:23 by ssharmaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+
+	va_start(args, format);
+	write(1, format, 10);
+	write(1, "\n", 1);
+	return (1);
+}
